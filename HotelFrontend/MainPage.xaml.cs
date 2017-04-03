53d0b37b8965ffd15867ad2a078a1e0339a97c32
Ditte <Ditte@DESKTOP-QORFADN>
2017-04-03 22:30:07 +0200

    HotelViewModel added with property & constructor and Facade class added

--- HotelFrontend/MainPage.xaml.cs
+++ HotelFrontend/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelFrontend.Connection;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,6 +26,7 @@ namespace HotelFrontend
         public MainPage()
         {
             this.InitializeComponent();
+
         }
     }
 }
---
+++ HotelFrontend/ViewModel/HotelViewModel.cs
@@ -0,0 +1,44 @@
+﻿using HotelFrontend.Connection;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFrontend.ViewModel
+{
+    public class HotelViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Guest> guestList;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string PropertyName)
+        {
+            if (PropertyName != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+            }
+        }
+
+        public ObservableCollection<Guest> GuestList
+        {
+            get { return guestList; }
+            set
+            {
+                guestList = value;
+                OnPropertyChanged(nameof(GuestList));
+            }
+        }
+
+        public HotelViewModel()
+        {
+            Facade facade = new Facade();
+            GuestList = facade.GetAllGuests().Result;
+        }
+
+
+    }
+}
